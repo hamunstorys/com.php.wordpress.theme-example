ccb0b3d09628e44d7fe2f3ca1ca606a53b85c390
Hong Jin-seob <hamunstorys@gmail.com>
2017-09-14 14:24:28 +0900

    set default layout in style.css

--- index.php
+++ index.php
@@ -7,6 +7,22 @@
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+<div id="container">
+    <div id="header">
+        <h1>Site Title</h1>
+    </div>
+    <div id="main">
+        <div id="content">
+            <h2>Content</h2>
+        </div>
+        <div id="sidebar">
+            <h2>Menu</h2>
+        </div>
+    </div>
+    <div id="footer">
+        <h3>Copyright</h3>
+    </div>
+</div>
 <?php wp_footer(); ?>
 </body>
 </html>

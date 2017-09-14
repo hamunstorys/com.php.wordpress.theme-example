68627980bfb3c7d001f02dbc29d663570b6db816
Hong Jin-seob <hamunstorys@gmail.com>
2017-09-14 15:33:40 +0900

    add functins.php file and add thumnails feature index.php

--- index.php
+++ index.php
@@ -18,19 +18,28 @@
     </div>
     <div id="main">
         <div id="content">
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <h2>
-                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                </h2>
-                <p>
-					<?php echo get_the_date(); ?> <?php echo get_the_time(); ?>
-                    | Category : <?php the_category( ', ' ); ?>
-                </p>
-				<?php if ( is_home() || is_category() || is_tag() ) {
-					the_excerpt();
-				} else {
-					the_content();
-				} ?>
+			<?php if ( have_posts() ) : while ( have_posts() ) :
+				the_post(); ?>
+                <div class="jb-post-list">
+                    <h2>
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </h2>
+                    <p>
+						<?php echo get_the_date(); ?> <?php echo get_the_time(); ?>
+                        | Category : <?php the_category( ', ' ); ?>
+                    </p>
+					<?php if ( is_home() || is_category() || is_tag() ) {
+
+						if ( has_post_thumbnail() ) {
+							the_post_thumbnail( array( 100, 100 ), array(
+								'class' => 'jb-thumbnail',
+							) );
+						}
+						the_excerpt();
+					} else {
+						the_content();
+					} ?>
+                </div>
 			<?php endwhile; else: ?>
                 <h2>Sorry!</h2>
 			<?php endif; ?>

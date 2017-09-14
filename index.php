7f1071c0decd9cb0bdf9a15682413eed5d9a8328
Hong Jin-seob <hamunstorys@gmail.com>
2017-09-14 15:22:56 +0900

    edit footer in index.php

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
         </div>
     </div>
     <div id="footer">
-        <h3>Copyright</h3>
+        <h3>Copyright <?php bloginfo( 'name' ); ?> All Rights Reserved.</h3>
     </div>
 </div>
 <?php wp_footer(); ?>

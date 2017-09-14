7093b5cb5ea76a3d677ff58b48969eae15d45eb2
Hong Jin-seob <hamunstorys@gmail.com>
2017-09-14 17:11:00 +0900

    debug overlap widget sidebar in index.php

--- index.php
+++ index.php
@@ -1,34 +1,28 @@
 <?php get_header(); ?>
-<div id="main">
-    <div id="content">
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <div class="jb-post-list">
-                <h2>
-                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                </h2>
-                <p>
-					<?php echo get_the_date(); ?> <?php echo get_the_time(); ?>
-                    | Category : <?php the_category(', '); ?>
-                </p>
-				<?php
-				if ( has_post_thumbnail() ) {
-					the_post_thumbnail( array(100,100), array(
-						'class' => 'jb-thumbnail',
-					) );
-				}
-				?>
-				<?php the_excerpt(); ?>
-            </div>
-		<?php endwhile; else: ?>
-            <h2>Sorry!</h2>
-		<?php endif; ?>
-	    <?php
-	    wp_nav_menu( array(
-		    'theme_location' => 'main_menu',
-		    'fallback_cb'    => false,
-	    ) );
-	    ?>
+    <div id="main">
+        <div id="content">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <div class="jb-post-list">
+                    <h2>
+                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                    </h2>
+                    <p>
+						<?php echo get_the_date(); ?> <?php echo get_the_time(); ?>
+                        | Category : <?php the_category( ', ' ); ?>
+                    </p>
+					<?php
+					if ( has_post_thumbnail() ) {
+						the_post_thumbnail( array( 100, 100 ), array(
+							'class' => 'jb-thumbnail',
+						) );
+					}
+					?>
+					<?php the_excerpt(); ?>
+                </div>
+			<?php endwhile; else: ?>
+                <h2>Sorry!</h2>
+			<?php endif; ?>
+        </div>
+		<?php get_sidebar(); ?>
     </div>
-	<?php get_sidebar(); ?>
-</div>
 <?php get_footer(); ?>
